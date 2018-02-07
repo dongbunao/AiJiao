65933aa3f0009b2a1a3f9835302bfc2b5e317af7
ddxiao <809660773@qq.com>
2018-02-07 08:35:49 +0800

    完成对爱教网知识点库的抓取，修改绝对路径转json的编码问题

--- path2json.py
+++ path2json.py
@@ -3,6 +3,32 @@
 import os
 import json
 
+# # 把文件的目录结构转换成json格式
+# def read_directory(path, result):
+#     paths = os.listdir(path)
+#     for i, item in enumerate(paths):
+#         sub_path = os.path.join(path, item)
+#         if os.path.isdir(sub_path):
+#             result[item] = {}
+#             read_directory(sub_path, result[item])
+#         else:
+#             with open(item, 'r', encoding='utf-8') as f:
+#                 content = f.read()
+#                 result[item] = content
+#
+# if __name__ == '__main__':
+#     fpath = 'E:/spAiJiao'   # 这里的路径需要时要转换的目录的顶层目录
+#     filename = 'E:/spAiJiao/json_res.json'   # 转换后的结果文件名称和存放路径
+#     result = {}
+#     read_directory(fpath, result)
+#     json_res = json.dumps(result, ensure_ascii=False, indent=2)   # ensure_ascii=False 用来解决中文乱码
+#     print(json_res)
+#     with open(filename, 'w', encoding='utf-8') as fp:
+#         fp.write(json_res)
+
+
+
+
 # 把文件的目录结构转换成json格式
 def read_directory(path, result):
     paths = os.listdir(path)
@@ -15,11 +41,11 @@ def read_directory(path, result):
             result[item] = item
 
 if __name__ == '__main__':
-    fpath = 'e:/spider1'   # 这里的路径需要时要转换的目录的顶层目录
-    filename = 'e:/spider1/json_res.json'   # 转换后的结果文件名称和存放路径
+    fpath = 'E:/spAiJiao'   # 这里的路径需要时要转换的目录的顶层目录
+    filename = 'E:/spAiJiao/json_res.json'   # 转换后的结果文件名称和存放路径
     result = {}
     read_directory(fpath, result)
     json_res = json.dumps(result, ensure_ascii=False, indent=2)   # ensure_ascii=False 用来解决中文乱码
     print(json_res)
-    with open(filename, 'w') as fp:
+    with open(filename, 'w', encoding='utf-8') as fp:
         fp.write(json_res)
--- spiderAJ_ZSD.py
+++ spiderAJ_ZSD.py
@@ -4,6 +4,8 @@ from pyquery import PyQuery as pq
 from fake_useragent import UserAgent
 import requests
 import os
+import re
+import time
 
 ua = UserAgent()
 
@@ -42,7 +44,6 @@ def get_subject(url):
         print('请求年级的学科页面出现异常：', e.args)
         return None
 
-
 def parse_subject(html):
     doc = pq(html)
     items = doc('body > div.bj > div.hroup.w > div.module > div.fl > div.list_screen > div:nth-child(3) > dl > dd a').items()
@@ -52,6 +53,54 @@ def parse_subject(html):
             'link': item.attr('href')
         }
 
+# 返回资源列表总的页数 pageNum
+def get_pageNum(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            doc = pq(response.text)
+            total = doc('body > div.bj > div.hroup.w > div.module > div.fl > div.mokao_list > div.refresh > div.page > span:nth-child(1) > b').text()
+            print('资源总数： ' + total)
+            if total == '0':
+                return 0
+
+            pageNum = doc('body > div.bj > div.hroup.w > div.module > div.fl > div.mokao_list > div.pageBar > span > b')  # 页数
+            print(pageNum.text())  # <b>第8/265页</b>
+            pattern = re.compile(r'.*?/(.*?)页')
+            pageNum = int(re.findall(pattern, str(pageNum))[0])
+            print(pageNum)
+            return pageNum
+        else:
+            print('请求资源总数和页数出错', response.status_code)
+            return 0
+    except Exception as e:
+        print('请求资源总数和页数出现异常', e.args)
+        return 0
+
+def get_zsd_list(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        else:
+            print('请求资源列表页出错', response.status_code)
+            time.sleep(10)
+            return get_zsd_list(url)
+    except Exception as e:
+        print('请求资源列表页出现异常', e.args)
+        return None
+
+def parse_zsd_list(html):
+    doc = pq(html)
+    items = doc('body > div.bj > div.hroup.w > div.module > div.fl > div.mokao_list > div.mokao_zsdk > ul li').items()
+    for item in items:
+        yield {
+            'title': item.find('.book_comm h3 a').text(),
+            'link': item.find('.book_comm h3 a').attr('href'),
+            'content': item.find('.book_comm .doc_info').text(),
+            'tag': item.find('.book_comm .tag .orange').text(),
+            'heat': item.find('.book_comm .tag .blue').text()
+        }
 
 def main():
     baseUrl = 'http://zyk.ajiao.com'
@@ -60,22 +109,50 @@ def main():
     nianjis = parse_index(html)
     for nianji in nianjis:
         print(nianji)
-        path1 = os.path.join('e:\\spAiJiao\\', nianji['nname'])
-        isExists = os.path.exists(path1)
-        if not isExists:
-            os.makedirs(path1)
-
-        #  已经建立年级文件夹，下面根据年级遍历学科
-        subUrl = baseUrl + nianji['link']
-        html = get_subject(subUrl)
-        subjects = parse_subject(html)
-        for subject in subjects:
-            print(subject)
-            path2 = os.path.join(path1, subject['subname'])
-            isExists = os.path.exists(path2)
+        if nianji['nname'] == '一年级' or nianji['nname'] == '二年级' or nianji['nname'] == '三年级' or nianji['nname'] == '四年级' or nianji['nname'] == '五年级' or nianji['nname'] == '六年级':
+            pass
+        else:
+            path1 = os.path.join('e:\\spAiJiao\\', nianji['nname'])
+            isExists = os.path.exists(path1)
             if not isExists:
-                os.makedirs(path2)
+                os.makedirs(path1)
+            #  已经建立年级文件夹，下面根据年级遍历学科
+            nianjiUrl = baseUrl + nianji['link']
+            html = get_subject(nianjiUrl)
+            subjects = parse_subject(html)
+            for subject in subjects:
+                print(subject)
+                path2 = os.path.join(path1, subject['subname'])
+                isExists = os.path.exists(path2)
+                if not isExists:
+                    os.makedirs(path2)
+                # 已经在班级文件夹下建立学科文件夹，遍历选定班级学科的知识点列表（分页）
+                subUrl = baseUrl + subject['link']
 
+                pageNum = get_pageNum(subUrl)
+                for x in range(1, pageNum + 1):
+                    nextUrl = subUrl + str(x)
+
+                    print('nextUrl: ' + nextUrl)
+
+                    html = get_zsd_list(nextUrl)
+                    zsds = parse_zsd_list(html)
+                    for zsd in zsds:
+                        print(zsd['title'])
+                        # 如果在知识点列表页提取内容，这里就不再请求和解析详情页
+                        table = str.maketrans("|\\?*<\":>+[]/'", '_' * 13)
+                        zsd['title'] = zsd['title'].translate(table)
+                        file_name = os.path.join(path2, zsd['title'] + '.txt')
+                        isExists = os.path.exists(file_name)
+
+                        if not isExists:  # 文件不存在才下载
+                            # os.makedirs(file_name)
+                            # u = urllib.request.urlopen(baseurl + downLink)
+                            with open(file_name, 'w', encoding='utf-8') as f:
+                                f.write(zsd['content'])
+                                print("Sucessful to download" + " " + file_name)
+                        else:
+                            print(file_name + '文件已经存在，不再重复下载。')
 
 
 
